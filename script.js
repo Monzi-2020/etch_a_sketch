dde46ec3d0027a1bfda2fa2a09206aabc39aaf93
Monzi2020 <nyenuen@gmail.com>
2024-03-08 21:34:42 +1100

    Added grid buttons for changing grid size. Added window.onload() to keep 16x16 grid as default Created gridBtns variable for buttons. Buttons work but need to reset the grid size since it just add new grid onto current grid

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const rows = 16;
-const columns = 16;
-
+const gridBtns = document.querySelectorAll('.gridBtn');
 const gridContainer = document.getElementById('container');
 
-for(let i= 0; i < rows; i++) {
+function makeGrid(rows, columns) {
+    for(let i= 0; i < rows; i++) {
     for(let j = 0; j < columns; j++) {
         const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item')
+        gridItem.classList.add('grid-item');
+        gridItem.setAttribute('style', `width: ${480/rows}px;` );
         gridContainer.appendChild(gridItem);
     }
 }
+}
+
+window.onload = function() {
+    makeGrid(16,16);
+}
+
+gridBtns.forEach((gridBtn) => {
+    gridBtn.addEventListener('click',() => {
+        const lows = gridBtn.id;
+        const columns = gridBtn.id;
+        makeGrid(lows, columns);
+    })
+})
+
+
+
