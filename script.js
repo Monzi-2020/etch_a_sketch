8efc76c831f646be68ebb6c207502f8f776ce7cd
Monzi2020 <nyenuen@gmail.com>
2024-03-11 21:08:00 +1100

    Added hover eventListner to javascript and hover class to css

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 const gridBtns = document.querySelectorAll('.gridBtn');
 const gridContainer = document.getElementById('container');
 
+window.onload = function() {
+    makeGrid(16,16);
+
+}
+
 function makeGrid(rows, columns) {
     for(let i= 0; i < rows; i++) {
     for(let j = 0; j < columns; j++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        gridItem.setAttribute('style', `width: ${480/rows}px;` );
+        gridItem.setAttribute('style', `width: ${960/rows}px;` );
         gridContainer.appendChild(gridItem);
+        
+        gridItem.addEventListener("mouseover", () => {
+        gridItem.classList.toggle("hover");
+        })
     }
-}
-}
-
-window.onload = function() {
-    makeGrid(16,16);
-}
+};}
 
 gridBtns.forEach((gridBtn) => {
-    gridBtn.addEventListener('click',() => {
+gridBtn.addEventListener('click',() => {
+        gridContainer.removeChild(gridItem);
         const lows = gridBtn.id;
         const columns = gridBtn.id;
         makeGrid(lows, columns);
     })
 })
-
 
 
