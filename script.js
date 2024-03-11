d69a24672f8243d9d0a8b3ed3b83b5dd5769f8fe
Monzi2020 <nyenuen@gmail.com>
2024-03-11 23:19:07 +1100

    Added get random color when mouse hovers Created randomInt() and randomRgb() in order to get random numbers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridBtns = document.querySelectorAll('.gridBtn');
 const gridContainer = document.querySelector('#container');
 const gridChange = document.querySelector('#gridChange');
+const eraser = document.querySelector('#eraser');
 
 window.onload = function() {
     makeGrid(16,16);
@@ -13,6 +14,7 @@ gridChange.addEventListener('click', () => {
     makeGrid(num,num);
 })
 
+
 function makeGrid(rows, columns) {
     for(let i= 0; i < rows; i++) {
     for(let j = 0; j < columns; j++) {
@@ -22,9 +24,20 @@ function makeGrid(rows, columns) {
         gridContainer.appendChild(gridItem);
         
         gridItem.addEventListener("mouseover", () => {
-        gridItem.classList.toggle("hover");
-        })
+        let rgb = randomRgb()
+        gridItem.setAttribute("style", `width: ${960/rows}px; 
+        background: rgb(${rgb});`);})
     }
 };}
 
+function randomInt(max) {
+    return Math.floor(Math.random()*(max +1));
+}
+
+function randomRgb() {
+    let r = randomInt(255);
+    let g = randomInt(255);
+    let b = randomInt(255);
+    return [r,g,b];
+}
 
