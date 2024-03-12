13ebfb21403220a3288ab73f1bbfaf9be136ee8b
Monzi2020 <nyenuen@gmail.com>
2024-03-12 17:02:41 +1100

    Created clear and eraser button Added mouseover and mouseout listener to buttons. Edited range slide bar's design into more retro

--- script.js
+++ script.js
@@ -3,19 +3,44 @@ const gridContainer = document.querySelector('#container');
 const changeGrid = document.querySelector('#changeGrid');
 const gridRange = document.querySelector('#gridRange')
 const rangeValue = document.querySelector('#rangeValue')
+const clear = document.querySelector('#clear');
+const eraser = document.querySelector('#eraser');
+const colorMode = document.querySelector('#colorMode');
+let num = gridRange.value;
 
 window.onload = function() {
     makeGrid(16,16);
 }
 
 gridRange.addEventListener('input', () => {
-    let num = gridRange.value;
+    num = gridRange.value;
     rangeValue.textContent = `${num}x${num}`
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach((item) => item.remove());
     makeGrid(num,num);
 })
 
+
+clear.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) => item.setAttribute('style', `width: ${960/num}px; background: white;`));
+});
+
+clear.addEventListener('mouseover', () => {
+    clear.classList.add("hover");
+})
+
+clear.addEventListener('mouseout', () => {
+    clear.classList.remove("hover");
+})
+
+eraser.addEventListener('mouseover', () => {
+    eraser.classList.add("hover");
+})
+
+eraser.addEventListener('mouseout', () => {
+    eraser.classList.remove("hover");
+})
 
 function makeGrid(rows, columns) {
     for(let i= 0; i < rows; i++) {
@@ -25,12 +50,20 @@ function makeGrid(rows, columns) {
         gridItem.setAttribute('style', `width: ${960/rows}px;` );
         gridContainer.appendChild(gridItem);
         
-        gridItem.addEventListener("mouseover", () => {
+        gridItem.addEventListener('mouseover', () => {
         let rgb = randomRgb()
-        gridItem.setAttribute("style", `width: ${960/rows}px; 
+        gridItem.setAttribute('style', `width: ${960/rows}px; 
         background: rgb(${rgb});`);})
-    }
-};}
+        
+        eraser.addEventListener('click', () => {
+            eraser.textContent = "Drawing"
+            gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('style',`width: ${960/num}px; background: white;`)});
+        });
+    }};
+}
+
+
 
 function randomInt(max) {
     return Math.floor(Math.random()*(max +1));
@@ -42,4 +75,3 @@ function randomRgb() {
     let b = randomInt(255);
     return [r,g,b];
 }
-
