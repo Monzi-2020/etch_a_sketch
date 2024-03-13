0aed099a21e831e993df380c6e17939c062f476e
Monzi2020 <nyenuen@gmail.com>
2024-03-13 11:35:50 +1100

    Made colorMode and earser button work Used nodelist instead of single node to select the grid-items

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ const eraser = document.querySelector('#eraser');
 const colorMode = document.querySelector('#colorMode');
 let num = gridRange.value;
 
-window.onload = function() {
-    makeGrid(16,16);
-}
 
 gridRange.addEventListener('input', () => {
     num = gridRange.value;
@@ -25,6 +22,78 @@ clear.addEventListener('click', () => {
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach((item) => item.setAttribute('style', `width: ${960/num}px; background: white;`));
 });
+
+colorMode.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) =>
+    item.addEventListener('mouseover', ()=>{
+    item.setAttribute('style', `width: ${960/num}px; background: black;`)
+    }))
+})
+
+eraser.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) =>
+    item.addEventListener('mouseover', ()=>{
+    item.setAttribute('style', `width: ${960/num}px; background: white;`)
+    }))
+})
+   
+
+// function changeMode() {
+//     if(eraser.className === "eraser") {
+//         eraser.classList.add("drawing")
+//         eraser.textContent = "Drawing";
+//         const gridItem = document.querySelector('.grid-item')
+//         gridItem.addEventListener('mouseover', () => {
+//         gridItem.setAttribute('style', `width: ${960/num}px; 
+//             background: black;)`)
+//         });
+//     }
+//     else {
+//         eraser.classList.add("eraser");
+//         eraser.textContent = "Eraser";
+//         const gridItem = document.querySelector('.grid-item')
+//         gridItem.addEventListener('mouseover', () => {
+//         gridItem.setAttribute('style', `width: ${960/num}px; 
+//             background: white;)`)
+//         });
+//     }
+
+// }
+
+function makeGrid(rows, columns) {
+    for(let i= 0; i < rows; i++) {
+    for(let j = 0; j < columns; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.setAttribute('style', `width: ${960/rows}px;` );
+        gridContainer.appendChild(gridItem);
+        
+        gridItem.addEventListener('mouseover', () => {
+        let rgb = randomRgb()
+        gridItem.setAttribute('style', `width: ${960/rows}px; 
+        background: rgb(${rgb});`);})
+
+
+    }};
+}
+
+
+window.onload = function() {
+    makeGrid(16,16);
+}
+
+function randomInt(max) {
+    return Math.floor(Math.random()*(max +1));
+}
+
+function randomRgb() {
+    let r = randomInt(255);
+    let g = randomInt(255);
+    let b = randomInt(255);
+    return [r,g,b];
+}
 
 clear.addEventListener('mouseover', () => {
     clear.classList.add("hover");
@@ -41,37 +110,3 @@ eraser.addEventListener('mouseover', () => {
 eraser.addEventListener('mouseout', () => {
     eraser.classList.remove("hover");
 })
-
-function makeGrid(rows, columns) {
-    for(let i= 0; i < rows; i++) {
-    for(let j = 0; j < columns; j++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.setAttribute('style', `width: ${960/rows}px;` );
-        gridContainer.appendChild(gridItem);
-        
-        gridItem.addEventListener('mouseover', () => {
-        let rgb = randomRgb()
-        gridItem.setAttribute('style', `width: ${960/rows}px; 
-        background: rgb(${rgb});`);})
-        
-        eraser.addEventListener('click', () => {
-            eraser.textContent = "Drawing"
-            gridItem.addEventListener('mouseover', () => {
-            gridItem.setAttribute('style',`width: ${960/num}px; background: white;`)});
-        });
-    }};
-}
-
-
-
-function randomInt(max) {
-    return Math.floor(Math.random()*(max +1));
-}
-
-function randomRgb() {
-    let r = randomInt(255);
-    let g = randomInt(255);
-    let b = randomInt(255);
-    return [r,g,b];
-}
